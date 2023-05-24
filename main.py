10ac1202f4a6f693a49b3d4bdbe9c51e24ecb4da
JanisChiffelle <janischiffelle@hotmail.ch>
2023-05-24 21:41:06 +0200

    wip refractor

--- main.py
+++ main.py
@@ -5,39 +5,42 @@ import pyarrow as pa
 from datetime import datetime, timedelta
 from dateutil import rrule
 import gcsfs
+import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
 
 bucket_name = "prediswiss-parquet-data"
 
+class Arima:
+    def __init__(self, bucket_name, datasets):
+        fs_gcs = gcsfs.GCSFileSystem()
+        dataframes = []
+        for dataset in datasets:
+            path = bucket_name + "/" + dataset + ".parquet"
+            table = pq.read_table(path, filesystem=fs_gcs)
+            df = table.to_pandas()
+            dataframes.append(df)
+        self.df = pd.concat(dataframes)
+
+    def train_model(self, id, target, order):
+        self.target = target
+        self.id = id
+        self.order = order
+        flow_df = self.df[self.df['id'] == id]
+        flow_df = flow_df[target]
+        flow_df = flow_df.fillna(value=0, inplace=True)
+        model = ARIMA(flow_df, order=order)
+        self.model_fit = model.fit()
+
+    def forecast(self, time):
+        forecast = self.model_fit.forecast(time)
+        print(forecast)
+
 def main():
-    datasetPath = "2023-05"
-    fs_gcs = gcsfs.GCSFileSystem()
-    path = bucket_name + "/" + datasetPath + ".parquet"
-    table = pq.read_table(path, filesystem=fs_gcs)
-    df = table.to_pandas()
-
-
-    #print(df.groupby(["publication_date"]).count())
-    with open('test.txt', 'w') as f:
-        f.write(df.groupby(["publication_date"]).count().to_string())
-
-    
-    flow_df = df[df['id'] == 'CH:0542.05']
-    flow_df = flow_df[['flow_11']]
-    flow_df = flow_df.dropna().values.astype(int)
-    print(flow_df)
-
-    # Fit an ARIMA model to the specified column
-    model = ARIMA(flow_df, order=(1,1,1))
-    
-    model_fit = model.fit()
-    
-    # Make predictions for the specified number of periods into the future
-    forecast = model_fit.forecast(10)
-
-    print(forecast)
+    arima = Arima(bucket_name, ["2023-05"])
+    arima.train_model('CH:0542.05', 'flow_1', (1,1,3))
+    arima.forecast(10)
 
 if __name__ == "__main__":
     main()
